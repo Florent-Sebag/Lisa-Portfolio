86f06d29659cb85a3418fede570acc5f93bff433
Sebag <octhave@gmail.com>
2020-07-08 15:52:08 +0200

    Add all more/web components

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -8,6 +8,10 @@ import { CmxRaceComponent } from '../pages/id-visu/more/cmx-race/cmx-race.compon
 import { SaintFideleComponent } from '../pages/id-visu/more/saint-fidele/saint-fidele.component';
 import { TypographieComponent } from '../pages/id-visu/more/typographie/typographie.component';
 import { WorldPastaDayComponent } from '../pages/id-visu/more/world-pasta-day/world-pasta-day.component';
+import { BambouComponent } from '../pages/web/more/bambou/bambou.component';
+import { EmailingComponent } from '../pages/web/more/emailing/emailing.component';
+import { MbrComponent } from '../pages/web/more/mbr/mbr.component';
+import { SuneliaComponent } from '../pages/web/more/sunelia/sunelia.component';
 
 export const appRouteList: Routes = [
   { path: '', component: HomeComponent },
@@ -23,4 +27,9 @@ export const appRouteList: Routes = [
   { path: 'portfolio/identites-visuelles/more/world-pasta-day', component: WorldPastaDayComponent },
 
   { path: 'portfolio/web', component: WebComponent },
+  { path: 'portfolio/web/more/bambouseraie', component: BambouComponent },
+  { path: 'portfolio/web/more/emailing', component: EmailingComponent },
+  { path: 'portfolio/web/more/mbr', component: MbrComponent },
+  { path: 'portfolio/web/more/sunelia', component: SuneliaComponent },
+
 ];
